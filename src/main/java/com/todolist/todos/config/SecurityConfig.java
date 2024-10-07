866a086a1cf3f866d82c766a5715df1dd206829f
StarLord🧑‍🎤 <star@lord.es>
2024-10-06 22:13:24 -0500

    security filter chain added

--- src/main/java/com/todolist/todos/config/SecurityConfig.java
+++ src/main/java/com/todolist/todos/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.todolist.todos.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    //Primero configuramos los end points publicos
+                    http.requestMatchers(HttpMethod.GET, "/api/auth").permitAll();
+                })
+                .build();
     }
 }
